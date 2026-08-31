929cf7947f47889027dd70d14aba1d9a27aaf889
Ananda Muhammad <39816224+handa26@users.noreply.github.com>
2026-08-31 15:26:01 +0800

    feat: fetch default board & columns data, slicing kanban & add job form page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,42 @@
-export default function Dashboard() {
-  return (
-    <div>
-      Dashboard Page
-    </div>
-  );
+import { redirect } from "next/navigation";
+
+import KanbanBoard from "@/components/kanban-board";
+
+import connectDB from "@/lib/db";
+import { Board } from "@/lib/models";
+import { getSession } from "@/lib/auth/auth";
+
+export default async function Dashboard() {
+	const session = await getSession();
+
+	if (!session?.user) {
+		redirect("/sign-in");
+	}
+
+	await connectDB();
+
+	const doc = await Board.findOne({
+		userId: session.user.id,
+		name: "Job Hunt",
+	}).populate({
+		path: "columns",
+	});
+
+	const board = doc.toObject({
+		flattenObjectIds: true, // Automatically converts _id to string
+		flattenMaps: true,
+	});
+
+	return (
+		<div className="min-h-screen bg-white">
+			<div className="container mx-auto p-6">
+				<div className="mb-6">
+					<h1 className="text-3xl font-bold text-black">{board.name}</h1>
+					<p className="text-gray-600">Track your job applications</p>
+				</div>
+
+				<KanbanBoard board={board} userId={session.user.id} />
+			</div>
+		</div>
+	);
 }
